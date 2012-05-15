7fd21c1d4a07e4294a0cb8754af24db06141905e
Tiendil <a.eletsky@gmail.com>
2012-05-15 13:06:34 +0300

    add functionality for working with pid files

--- dext/utils/pid.py
+++ dext/utils/pid.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 import os
+from contextlib import contextmanager
 
 from dext.utils.conf import utils_settings
 
@@ -14,7 +15,7 @@ def capture(uuid, directory=utils_settings.PID_DIRECTORY, directory_mode=utils_s
         return False
 
     with open(pid_file, 'w') as f:
-        f.write('%d' % os.getpid())
+        f.write('%d\n' % os.getpid())
 
     return True
 
@@ -22,3 +23,19 @@ def capture(uuid, directory=utils_settings.PID_DIRECTORY, directory_mode=utils_s
 def check(uuid, directory=utils_settings.PID_DIRECTORY):
     pid_file = os.path.join(directory, uuid + '.pid')
     return os.path.exists(pid_file)
+
+
+def free(uuid, directory=utils_settings.PID_DIRECTORY):
+    pid_file = os.path.join(directory, uuid + '.pid')
+    os.remove(pid_file)
+
+
+@contextmanager
+def wrap(uuid, directory=utils_settings.PID_DIRECTORY, directory_mode=utils_settings.PID_DIRECTORY_MODE):
+    if not capture(uuid, directory, directory_mode):
+        print 'process has been already running'
+        yield
+        return
+
+    yield
+    free(uuid, directory)
