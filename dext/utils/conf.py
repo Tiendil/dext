e4e1573913d14c45ccc6a7d1cb60404555a783ff
Tiendil <a.eletsky@gmail.com>
2012-06-05 16:14:51 +0300

    add functionality for working with pid files

--- dext/utils/conf.py
+++ dext/utils/conf.py
@@ -1,8 +1,6 @@
 # coding: utf-8
-import os
-
 from dext.utils.app_settings import app_settings
 
 utils_settings = app_settings('DEXT',
-                              PID_DIRECTORY=os.getenv("HOME"),
+                              PID_DIRECTORY='/tmp',
                               PID_DIRECTORY_MODE=0755)
