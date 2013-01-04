b9edeb290d92129c38421abd25d4a6e273195844
Tiendil <a.eletsky@gmail.com>
2013-01-05 00:05:23 +0300

    add meta_config module to utils

--- dext/less/urls.py
+++ dext/less/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, url
 
-from .views import less_compiler
+from dext.less.views import less_compiler
 
 urlpatterns = patterns('',
                        url(r'^(?P<path>.*).css$', less_compiler),
