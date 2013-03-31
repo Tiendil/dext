fd985fa505d24491f3a48cc3e8716a2168c542f2
Tiendil <a.eletsky@gmail.com>
2013-03-31 14:22:17 +0300

    django 1.5.1 UrlBuilder now return full urls

--- dext/utils/urls.py
+++ dext/utils/urls.py
@@ -1,11 +1,13 @@
 # coding: utf8
 import copy
+from django.conf import settings as project_settings
 
 class UrlBuilder(object):
 
-    def __init__(self, base, arguments={}):
+    def __init__(self, base, protocol='http', arguments={}):
         self.base = base
         self.default_arguments = arguments
+        self.protocol = protocol
 
     @property
     def arguments_names(self): return self.default_arguments.keys()
@@ -17,4 +19,4 @@ class UrlBuilder(object):
         arguments = copy.copy(self.default_arguments)
         arguments.update(kwargs)
 
-        return self.base + '?' + '&'.join('%s=%s' % (key, value) for key, value in arguments.items() if value is not None)
+        return self.protocol + '://' + project_settings.SITE_URL + self.base + '?' + '&'.join('%s=%s' % (key, value) for key, value in arguments.items() if value is not None)
