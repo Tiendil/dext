0e97b9fdcb12a08ba7ed540e9878d83823962656
Tiendil <a.eletsky@gmail.com>
2013-05-27 20:17:03 +0300

    fix bug with reponse type detecting, when request does not contain content type.

--- dext/utils/response.py
+++ dext/utils/response.py
@@ -18,6 +18,10 @@ def template(template_name, context, request, mimetype='text/html'):
 
 
 def content_type_to_response_type(content_type):
+    if content_type is None:
+        return 'json'
+
     if any(tp in content_type for tp in ('application/xhtml+xml', 'text/html', 'text/plain', 'text/xml')):
         return 'html'
+
     return 'json'
