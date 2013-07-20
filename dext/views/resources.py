3ce4d798260e2bf9bb2fb629a2f95f473a170d47
Tiendil <a.eletsky@gmail.com>
2013-07-20 16:35:48 +0300

    add user_ip property to resource

--- dext/views/resources.py
+++ dext/views/resources.py
@@ -155,6 +155,19 @@ class BaseResource(object):
 
         return sorted(handlers.values())
 
+    @property
+    def user_ip(self):
+        """Returns the IP of the request, accounting for the possibility of being
+        behind a proxy.
+        """
+        ip = self.request.META.get("HTTP_X_FORWARDED_FOR", None)
+        if ip:
+            # X_FORWARDED_FOR returns client1, proxy1, proxy2,...
+            ip = ip.split(", ")[0]
+        else:
+            ip = self.request.META.get("REMOTE_ADDR", "")
+        return ip
+
     def string(self, string, charset='utf-8'):
         return HttpResponse(string, mimetype='text/html; charset=%s' % charset)
 
