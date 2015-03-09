02082f779525b1ec61e640e411b0f8b44dab94c2
Tiendil <a.eletsky@gmail.com>
2015-03-09 16:28:33 +0300

    workers: resert django db queries log before every command processing

--- dext/common/amqp_queues/workers.py
+++ dext/common/amqp_queues/workers.py
@@ -6,6 +6,7 @@ import Queue
 
 from django.conf import settings as project_settings
 from django.utils.log import getLogger
+from django import db
 
 from dext.common.amqp_queues import exceptions
 from dext.common.amqp_queues.connection import connection
@@ -65,6 +66,8 @@ class BaseWorker(object):
     def run(self):
         while not self.exception_raised and not self.stop_required:
             try:
+                db.reset_queries()
+
                 if self.GET_CMD_TIMEOUT > 0:
                     cmd = self.command_queue.get(block=True, timeout=self.GET_CMD_TIMEOUT)
                 else:
