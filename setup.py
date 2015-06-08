02805ca177a6d5e9f304fa842ca027bad25a719c
Tiendil <a.eletsky@gmail.com>
2015-06-08 13:22:49 +0300

    gh-7 Ввести версионность

--- setup.py
+++ setup.py
@@ -2,14 +2,37 @@
 import setuptools
 
 setuptools.setup(
-    name = 'Dext',
-    version = '0.1.0',
-    author = 'Aleksey Yeletsky',
-    author_email = 'a.eletsky@gmail.com',
-    packages = setuptools.find_packages(),
-    url = 'https://github.com/Tiendil/dext',
-    license = 'LICENSE',
-    description = "minor extentions for django",
-    long_description = open('README').read(),
-    include_package_data = True # setuptools-git MUST be installed
-)
+    name='Dext',
+    version='0.2.0',
+    description='minor extentions for django',
+    long_description = open('README.rst').read(),
+    url='https://github.com/Tiendil/dext',
+    author='Aleksey Yeletsky <Tiendil>',
+    author_email='a.eletsky@gmail.com',
+    license='BSD',
+    install_requires=[
+        "Django>=1.8",
+        "Jinja2>=2.6",
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        'Intended Audience :: Developers',
+
+        'Topic :: Games/Entertainment',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+        'License :: OSI Approved :: BSD License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+
+        'Framework :: Django :: 1.8',
+
+        'Natural Language :: English'],
+    keywords=['django'],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    test_suite = 'tests',
+    )
